59b391c5be8fbaf3012b5cb6bb152904dd6b9ec8
hieudm <minhhieu030598>
2019-09-06 10:10:07 +0700

    Create project

---
+++ src/main/groovy/com/libraryspringboot/LibraryspringbootApplication.groovy
@@ -0,0 +1,13 @@
+package com.libraryspringboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LibraryspringbootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LibraryspringbootApplication, args)
+    }
+
+}
