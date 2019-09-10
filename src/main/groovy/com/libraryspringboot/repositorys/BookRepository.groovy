4c4ef71fc2a185906e94ce81abaf97e2f9e3e824
hieudm <minhhieu030598>
2019-09-10 12:08:02 +0700

    Revert "LB-3 :Add feature Statistical"
    This reverts commit 75be92fcbbb12c302ac67837e49861c5e7dadde3.

--- src/main/groovy/com/libraryspringboot/repositorys/BookRepository.groovy
+++ src/main/groovy/com/libraryspringboot/repositorys/BookRepository.groovy
@@ -2,15 +2,9 @@ package com.libraryspringboot.repositorys
 
 import com.libraryspringboot.models.database.Book
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface BookRepository extends JpaRepository<Book, Integer> {
-
-    @Query(nativeQuery = true, value = 'SELECT * FROM books where status = 1 and is_use = 0')
-    List<Book> findAllNotUse()
-
-
 
 }
